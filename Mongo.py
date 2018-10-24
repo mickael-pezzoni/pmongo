114b5fd8e8b3031d1531886102c72e8dadd47d80
Mickael Pezzoni <pezzoni.mickael@gmail.com>
2018-10-24 11:31:08 +0200

    ajout export collection(s)

--- Mongo.py
+++ Mongo.py
@@ -1,28 +1,48 @@
 from pymongo import MongoClient
 import pprint
 
+
 class Mongo:
     """Classe pour une connection à une base mongo """
-    
+
     def __init__(self, host, port):
         self.host = host
         self.port = port
-        self.client = MongoClient(self.host,self.port)
-    
+        self.client = MongoClient(self.host, self.port)
+
     def showDataBase(self):
         for db in self.client.database_names():
-            print("*",db)
-    
+            print("*", db)
+
     def printCollection(self, base, collectionName):
         db = self.client[base]
+        content = str()
         collection = db[collectionName]
         for elt in collection.find():
-            pprint.pprint(elt)
-    
-    def showListCollection(self,base):
+            content += pprint.pformat(elt)+'\n'
+        return content
+
+    def showListCollection(self, base):
         db = self.client[base]
         nbCollection = 0
         for collection in db.collection_names():
-            nbCollection+=1
-            print("{} : {}".format(nbCollection,collection))
+            nbCollection += 1
+            print("{} : {}".format(nbCollection, collection))
         print("\n{} collections trouvées".format(nbCollection))
+
+    def exportAllCollectioni(self, base):
+        db = self.client[base]
+        nbCollection = 0
+        for collection in db.collection_names():
+            nbCollection += 1
+            with open(collection + ".json", "w+") as fichier:
+                print("Export " + collection + " ...")
+                fichier.write(self.printCollection(base, collection))
+        print("Export finit")
+
+    def exportOneCollection(self, base, collection):
+        db = self.client[base]
+        with open(collection + ".json", 'w+') as fichier:
+            print("Export " + collection + " ...")
+            fichier.write(self.printCollection(base, collection))
+            print("Export finit")
